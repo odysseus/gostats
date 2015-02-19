4580e4f4f0ee872c12dc6bd8816b2d65dbeb7597
Ryan Case <rmcase@gmail.com>
2015-02-18 18:11:51 -0700

    Moved stream processing into its own project

--- stats.go
+++ stats.go
@@ -7,33 +7,10 @@ import (
 
 func main() {
 	fmt.Println("Hello, stats!")
-}
-
-// Stream processing experiments
-func MeanStream(in, out chan float64) {
-	total := float64(0)
-	count := float64(0)
-	for {
-		newn := <-in
-		total += newn
-		count++
-		out <- (total / count)
-	}
-}
-
-type PartialMean struct {
-	total float64
-	count int
-}
-
-func CalcPartialMean(xs []float64) PartialMean {
-	total := float64(0)
-	count := 0
-	for _, f := range xs {
-		total += f
-		count++
-	}
-	return PartialMean{total, count}
+	fi := FloatIndex{}
+	fi.Put(3)
+	fi.Put(3)
+	fmt.Println(fi.Get(3))
 }
 
 // Basic statistical function on slices of float64
